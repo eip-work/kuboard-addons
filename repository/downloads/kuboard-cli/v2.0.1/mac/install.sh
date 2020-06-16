b6558c6f38852241cd91d929501c4befd251ec88
huanqing.shao <shaohq@foxmail.com>
2020-06-16 23:22:19 +0800

    v2.0.1-beta.3 支持 port-forward

--- repository/downloads/kuboard-cli/v2.0.1/mac/install.sh
+++ repository/downloads/kuboard-cli/v2.0.1/mac/install.sh
@@ -9,7 +9,11 @@ echo
 echo "install script loaded, start to download kuboard-cli binary."
 echo
 
-_do curl -O https://addons.kuboard.cn/downloads/kuboard-cli/v2.0.1/mac/kuboard-cli
+_do wget https://addons.kuboard.cn/downloads/kuboard-cli/v2.0.1/mac/kuboard-cli.zip.bin
+
+_do mv ./kuboard-cli.zip.bin ./kuboard-cli.zip
+
+_do unzip ./kuboard-cli.zip
 
 _do chmod a+x ./kuboard-cli
 
@@ -17,4 +21,9 @@ rm /usr/local/bin/kuboard-cli || true
 
 _do mv ./kuboard-cli /usr/local/bin
 
-echo "kuboard-cli installed at /usr/local/bin/kuboard-cli. Now, you can use it following the tips in Kuboard UI."
+rm ./kuboard-cli.zip
+
+echo
+echo
+
+echo "Congratulations! kuboard-cli is already installed at /usr/local/bin/kuboard-cli. Now, you can use it following the tips in Kuboard UI."
